5d5736fe69b850d8a17419161470299d8a43a7af
prem <prembabu.panchadara@iglulabs.com>
2014-12-18 10:48:12 +0530

    Implemented DatePicker

--- app/src/main/java/com/findmycoach/mentor/activity/SignUpActivity.java
+++ app/src/main/java/com/findmycoach/mentor/activity/SignUpActivity.java
@@ -1,40 +1,90 @@
 package com.findmycoach.mentor.activity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fmc.mentor.findmycoach.R;
 
+import java.util.Calendar;
+
 public class SignUpActivity extends Activity {
+
+
+    private int year, month, day;
+    private TextView dateOfBirth;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText password;
+    private EditText confirmPassword;
+    private EditText email;
+    private DatePickerDialog.OnDateSetListener myDateListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+            showDate(year, month + 1, date);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        initialize();
+    }
+
+    private void initialize() {
+        firstName = (EditText) findViewById(R.id.input_first_name);
+        lastName = (EditText) findViewById(R.id.input_last_name);
+        email = (EditText) findViewById(R.id.input_email);
+        password = (EditText) findViewById(R.id.input_password);
+        confirmPassword = (EditText) findViewById(R.id.input_confirm_password);
+        dateOfBirth = (TextView) findViewById(R.id.input_date_of_birth);
+
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private void showDate(int year, int month, int day) {
+        dateOfBirth.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_signup, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
