001ed49f41b8ec2c21e47d32b083b1d3a778f1e9
prem <prembabu.panchadara@iglulabs.com>
2015-01-07 14:58:29 +0530

    Implemented ForgetPassword

--- app/src/main/java/com/findmycoach/mentor/activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/findmycoach/mentor/activity/ForgotPasswordActivity.java
@@ -2,18 +2,26 @@ package com.findmycoach.mentor.activity;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.findmycoach.mentor.util.Callback;
+import com.findmycoach.mentor.util.NetworkClient;
 import com.fmc.mentor.findmycoach.R;
+import com.loopj.android.http.RequestParams;
 
-public class ForgotPasswordActivity extends Activity {
+public class ForgotPasswordActivity extends Activity implements Callback {
 
     private EditText emailInput;
     private Button submitButton;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +29,48 @@ public class ForgotPasswordActivity extends Activity {
         setContentView(R.layout.activity_forgot_password);
         applyActionbarProperties();
         initialize();
+        applyActions();
+    }
+
+    private void applyActions() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isInputValid()) {
+                    requestForgetPassword();
+                }
+            }
+        });
+    }
+
+    private void requestForgetPassword() {
+        progressDialog.show();
+        RequestParams requestParams = new RequestParams();
+        requestParams.add("email", emailInput.getText().toString());
+        NetworkClient.forgetPassword(this, requestParams, this);
+    }
+
+    private boolean isInputValid() {
+        String inputEmail = emailInput.getText().toString();
+        if (inputEmail.isEmpty()) {
+            emailInput.setError("Please enter valid email");
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    emailInput.setError(null);
+                }
+            }, 2500);
+
+            return false;
+        }
+        return true;
     }
 
     private void initialize() {
         emailInput = (EditText) findViewById(R.id.input_email);
         submitButton = (Button) findViewById(R.id.action_submit);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
     }
 
     private void applyActionbarProperties() {
@@ -51,5 +96,20 @@ public class ForgotPasswordActivity extends Activity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void successOperation(Object object) {
+        progressDialog.dismiss();
+        String message = (String) object;
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        this.finish();
+    }
+
+    @Override
+    public void failureOperation(Object object) {
+        progressDialog.dismiss();
+        String message = (String) object;
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/findmycoach/mentor/util/NetworkClient.java
+++ app/src/main/java/com/findmycoach/mentor/util/NetworkClient.java
@@ -12,6 +12,7 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.apache.http.Header;
+import org.json.JSONObject;
 
 /**
  * Created by prem on 18/12/14.
@@ -22,9 +23,14 @@ public class NetworkClient {
 
     private static String BASE_URL = "http://10.1.1.110/fmcweb/www/api/auth/";
 
+
+    public static String getAbsoluteURL(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
     public static void login(Context context, RequestParams requestParams, final Callback callback) {
         client.addHeader(context.getResources().getString(R.string.api_key), context.getResources().getString(R.string.api_key_value));
-        client.post(BASE_URL + "login", requestParams, new AsyncHttpResponseHandler() {
+        client.post(getAbsoluteURL("login"), requestParams, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 String responseJson = new String(responseBody);
@@ -52,7 +58,7 @@ public class NetworkClient {
 
     public static void register(Context context, RequestParams requestParams, final Callback callback) {
         client.addHeader(context.getResources().getString(R.string.api_key), context.getResources().getString(R.string.api_key_value));
-        client.post(context, BASE_URL + "register", requestParams, new AsyncHttpResponseHandler() {
+        client.post(context, getAbsoluteURL("register"), requestParams, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 String responseJson = new String(responseBody);
@@ -73,6 +79,35 @@ public class NetworkClient {
                 } catch (Exception e) {
                     Log.d("FMC", "Failure: Error:" + e.getMessage());
                     callback.failureOperation("Problem connecting to server");
+                }
+            }
+        });
+    }
+
+    public static void forgetPassword(Context context, RequestParams requestParams, final Callback callback) {
+        client.addHeader(context.getResources().getString(R.string.api_key), context.getResources().getString(R.string.api_key_value));
+        client.post(getAbsoluteURL("forgot_password"), requestParams, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    JSONObject jsonObject = new JSONObject(new String(responseBody));
+                    if (statusCode == 200) {
+                        callback.successOperation(jsonObject.get("message"));
+                    } else {
+                        callback.failureOperation(jsonObject.get("message"));
+                    }
+                } catch (Exception e) {
+                    callback.failureOperation("Problem connection to server");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                try {
+                    JSONObject jsonObject = new JSONObject(new String(responseBody));
+                    callback.failureOperation(jsonObject.get("message"));
+                } catch (Exception e) {
+                    callback.failureOperation("Problem connection to server");
                 }
             }
         });
