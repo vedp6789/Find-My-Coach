fd841b86b455e7825bf1516738952719f8c98e24
Ved Prakash <ved.prakash@iglulabs.com>
2015-02-20 13:50:59 +0530

    Manifest change

--- app/src/main/java/com/findmycoach/mentor/service/GcmFmcService.java
+++ app/src/main/java/com/findmycoach/mentor/service/GcmFmcService.java
@@ -50,14 +50,14 @@ public class GcmFmcService extends IntentService {
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
                 // This loop represents the service doing some work.
-                for (int i=0; i<5; i++) {
+                /*for (int i=0; i<5; i++) {
                     Log.i(TAG, "Working... " + (i + 1)
                             + "/5 @ " + SystemClock.elapsedRealtime());
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
                     }
-                }
+                }*/
                 Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
                 //sendNotification("Received: " + extras.toString());
