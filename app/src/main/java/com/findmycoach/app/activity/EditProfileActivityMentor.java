e05b046d4061e1411b8848d906c41bb09d194f7c
shekhar <shekhar114@gmail.com>
2015-04-28 20:49:01 +0530

    Bug fix

--- app/src/main/java/com/findmycoach/app/activity/EditProfileActivityMentor.java
+++ app/src/main/java/com/findmycoach/app/activity/EditProfileActivityMentor.java
@@ -209,7 +209,7 @@ public class EditProfileActivityMentor extends Activity implements DatePickerDia
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage(getResources().getString(R.string.please_wait));
 
-        if (userInfo.getAddress().toString().trim().equals("")) {
+        if (userInfo == null || userInfo.getAddress() == null || userInfo.getAddress().toString().trim().equals("")) {
             try {
                 Address fullAddress = NetworkManager.getFullAddres(this);
                 if (fullAddress != null) {
