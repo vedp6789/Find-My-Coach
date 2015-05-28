d0e2fa081bd4e249c5bddd5be0f40afd30446377
shekhar <shekhar114@gmail.com>
2015-05-28 16:52:02 +0530

    temp commit

--- app/src/main/java/com/findmycoach/app/adapter/MentorNotificationTabsPagerAdapter.java
+++ app/src/main/java/com/findmycoach/app/adapter/MentorNotificationTabsPagerAdapter.java
@@ -12,7 +12,6 @@ import com.findmycoach.app.fragment_mentor.ConnectionRequestFragment;
 import com.findmycoach.app.fragment_mentor.ScheduleRequestFragment;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by ved on 13/5/15.
@@ -21,24 +20,27 @@ public class MentorNotificationTabsPagerAdapter extends FragmentStatePagerAdapte
     MentorNotifications mentorNotifications;
     ArrayList<ScheduleRequest> scheduleRequest_notification_list;
     ArrayList<ConnectionRequest> connectionRequest_notificaton_list;
-    private String [] titles = {"Connection request","Schedule request"};
-    public MentorNotificationTabsPagerAdapter(FragmentManager fm,MentorNotifications mentorNotifications) {
+    private String[] titles = {"Connection request", "Schedule request"};
+
+    public MentorNotificationTabsPagerAdapter(FragmentManager fm, MentorNotifications mentorNotifications) {
         super(fm);
-        this.mentorNotifications=mentorNotifications;
-        scheduleRequest_notification_list=mentorNotifications.getList_of_schedule_request();
-        connectionRequest_notificaton_list=mentorNotifications.getList_of_connection_request();
+        this.mentorNotifications = mentorNotifications;
+        scheduleRequest_notification_list = mentorNotifications.getList_of_schedule_request();
+        connectionRequest_notificaton_list = mentorNotifications.getList_of_connection_request();
     }
-    private String TAG="FMC";
+
+    private String TAG = "FMC";
+
     @Override
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                Log.d(TAG,"connection_request_notifications");
+                Log.d(TAG, "connection_request_notifications");
                 return ConnectionRequestFragment.newInstance(connectionRequest_notificaton_list);
 
 
             case 1:
-                Log.d(TAG,"schedule_request_notifications");
+                Log.d(TAG, "schedule_request_notifications");
                 return ScheduleRequestFragment.newInstance(scheduleRequest_notification_list);
 
         }
