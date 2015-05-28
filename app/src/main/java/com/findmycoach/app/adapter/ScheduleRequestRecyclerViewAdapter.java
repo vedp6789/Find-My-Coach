1ae09dd6efd42b55e7618316e6eff3cde40a145a
shekhar <shekhar114@gmail.com>
2015-05-28 17:41:14 +0530

    Mentor Home themed

--- app/src/main/java/com/findmycoach/app/adapter/ScheduleRequestRecyclerViewAdapter.java
+++ app/src/main/java/com/findmycoach/app/adapter/ScheduleRequestRecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.findmycoach.app.adapter;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,32 +19,33 @@ import java.util.GregorianCalendar;
 /**
  * Created by ved on 14/5/15.
  */
-public class ScheduleRequestRecyclerViewAdapter extends RecyclerView.Adapter<ScheduleRequestRecyclerViewAdapter.ViewHolder>{
+public class ScheduleRequestRecyclerViewAdapter extends RecyclerView.Adapter<ScheduleRequestRecyclerViewAdapter.ViewHolder> {
 
     private ArrayList<ScheduleRequest> scheduleRequests;
     private Context context;
     private boolean no_schedule_request;
     private long days;
+
     public ScheduleRequestRecyclerViewAdapter(Context context, ArrayList<ScheduleRequest> scheduleRequests) {
-       this.context=context;
-       this.scheduleRequests=scheduleRequests;
-       no_schedule_request=false;
-        if(scheduleRequests.size() <=0){
-            no_schedule_request=true;
+        this.context = context;
+        this.scheduleRequests = scheduleRequests;
+        no_schedule_request = false;
+        if (scheduleRequests.size() <= 0) {
+            no_schedule_request = true;
         }
 
     }
 
-    public  class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         RelativeLayout relativeLayout;
         TextView tv_schedule_request_message;
         TextView tv_schedule_request_date_info;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            relativeLayout= (RelativeLayout) itemView.findViewById(R.id.rl_schedule_request_notification);
-            tv_schedule_request_message= (TextView) itemView.findViewById(R.id.tv_schedule_request_message);
-            tv_schedule_request_date_info= (TextView) itemView.findViewById(R.id.tv_how_old_schedule_request);
+            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.rl_schedule_request_notification);
+            tv_schedule_request_message = (TextView) itemView.findViewById(R.id.tv_schedule_request_message);
+            tv_schedule_request_date_info = (TextView) itemView.findViewById(R.id.tv_how_old_schedule_request);
 
         }
     }
@@ -53,28 +53,31 @@ public class ScheduleRequestRecyclerViewAdapter extends RecyclerView.Adapter<Sch
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.schedule_request_notification,parent,false);
-        ViewHolder viewHolder=new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.schedule_request_notification, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        if(no_schedule_request){
+        if (no_schedule_request) {
             Log.d("FMC", "no schedule request message !");
             holder.tv_schedule_request_message.setText(context.getResources().getString(R.string.no_schedule_request));
-        }else{
-            String first_name=scheduleRequests.get(position).getFirst_name();
-            String start_date=scheduleRequests.get(position).getStart_date();
-            String start_time=scheduleRequests.get(position).getStart_time();
-            String status=scheduleRequests.get(position).getStatus();
-            String created_date=scheduleRequests.get(position).getCreated_date();
+        } else {
+            String first_name = scheduleRequests.get(position).getFirst_name();
+            String start_date = scheduleRequests.get(position).getStart_date();
+            String start_time = scheduleRequests.get(position).getStart_time();
+            String status = scheduleRequests.get(position).getStatus();
+            String created_date = scheduleRequests.get(position).getCreated_date();
 
-            if(status.equals("read"))
-                holder.relativeLayout.setBackgroundColor(Color.LTGRAY);
+            if (!status.equals("read")) {
+                holder.relativeLayout.setBackgroundColor(context.getResources().getColor(R.color.purple_light));
+                holder.tv_schedule_request_message.setTextColor(context.getResources().getColor(R.color.white));
+                holder.tv_schedule_request_date_info.setTextColor(context.getResources().getColor(R.color.purple));
+            }
 
             Calendar cal = new GregorianCalendar();
-            cal.set(Integer.parseInt(created_date.split("-")[0]),Integer.parseInt(created_date.split("-")[1])-1, Integer.parseInt(created_date.split("-")[2]));
+            cal.set(Integer.parseInt(created_date.split("-")[0]), Integer.parseInt(created_date.split("-")[1]) - 1, Integer.parseInt(created_date.split("-")[2]));
             long connection_request_start_date_in_millis = cal.getTimeInMillis();
 
 
@@ -84,10 +87,10 @@ public class ScheduleRequestRecyclerViewAdapter extends RecyclerView.Adapter<Sch
             int current_minute = rightNow.get(Calendar.MINUTE);
 
 
-            long difference=rightNow_in_millis-connection_request_start_date_in_millis;
+            long difference = rightNow_in_millis - connection_request_start_date_in_millis;
             days = difference / (24 * 60 * 60 * 1000);
 
-            holder.tv_schedule_request_message.setText(first_name.trim() +" "+ context.getResources().getString(R.string.schedule_request_message));
+            holder.tv_schedule_request_message.setText(first_name.trim() + " " + context.getResources().getString(R.string.schedule_request_message));
             actionDay(holder);
 
         }
@@ -96,27 +99,25 @@ public class ScheduleRequestRecyclerViewAdapter extends RecyclerView.Adapter<Sch
 
     @Override
     public int getItemCount() {
-        if(no_schedule_request){
+        if (no_schedule_request) {
             return 1;
-        }else {
+        } else {
             return scheduleRequests.size();
         }
     }
 
 
     private void actionDay(ViewHolder holder) {
-        if(days == 0){
+        if (days == 0) {
             holder.tv_schedule_request_date_info.setText(context.getResources().getString(R.string.today));
-        }else{
-            if(days == 1){
+        } else {
+            if (days == 1) {
                 holder.tv_schedule_request_date_info.setText(context.getResources().getString(R.string.yesterday));
-            }else{
-                holder.tv_schedule_request_date_info.setText(days+context.getResources().getString(R.string.days_ago));
+            } else {
+                holder.tv_schedule_request_date_info.setText(days + " " + context.getResources().getString(R.string.days_ago));
             }
         }
     }
-
-
 
 
 }
