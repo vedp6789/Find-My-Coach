f07dc5635b1d49a728aa3e32142a710cc1ba68a2
shekhar <shekhar114@gmail.com>
2015-07-24 18:10:03 +0530

    DOB bug fix Area of coaching bug fix Signout from editprofile half fixed

--- app/src/main/java/com/findmycoach/app/activity/DashboardActivity.java
+++ app/src/main/java/com/findmycoach/app/activity/DashboardActivity.java
@@ -110,6 +110,7 @@ public class DashboardActivity extends FragmentActivity
     public double latitude;
     public double longitude;
     private GoogleMap.OnMyLocationChangeListener myLocationChangeListener;
+    private final int NEED_TO_SIGN_OUT = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -286,12 +287,12 @@ public class DashboardActivity extends FragmentActivity
                 ProfileResponse response = (ProfileResponse) object;
                 Intent intent = new Intent(this, EditProfileActivityMentee.class);
                 intent.putExtra("user_info", new Gson().toJson(response.getData()));
-                startActivity(intent);
+                startActivityForResult(intent, NEED_TO_SIGN_OUT);
             } else if (user_group == 3) {
                 Response response = (Response) object;
                 Intent intent = new Intent(this, EditProfileActivityMentor.class);
                 intent.putExtra("user_info", new Gson().toJson(response.getData()));
-                startActivity(intent);
+                startActivityForResult(intent, NEED_TO_SIGN_OUT);
             }
             isProfileOpen = false;
         }
@@ -375,8 +376,11 @@ public class DashboardActivity extends FragmentActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        Log.d(TAG, "onActivityResult dashboard");
+        Log.e(TAG, requestCode + " : " + resultCode);
+        Log.e(TAG, "onActivityResult dashboard");
+        if(resultCode == RESULT_OK && requestCode == NEED_TO_SIGN_OUT){
+            logout();
+        }
     }
 
     @Override
@@ -495,9 +499,11 @@ public class DashboardActivity extends FragmentActivity
 
 
     public void logout() {
+        Log.e(TAG, "Logout");
         String loginWith = StorageHelper.getUserDetails(this, "login_with");
         if (loginWith == null || loginWith.equals("G+")) {
             LoginActivity.doLogout = true;
+            Log.e(TAG, "Logout G+ true");
         }
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -845,6 +851,7 @@ public class DashboardActivity extends FragmentActivity
     }
 
     public void fbClearToken() {
+        Log.e(TAG, "Logout fb clear");
         Session session = Session.getActiveSession();
         if (session != null) {
             if (!session.isClosed()) {
