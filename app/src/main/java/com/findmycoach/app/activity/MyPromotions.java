73b6ac3e4357ec696c3319e21c0baeda425e836a
Ved <ved.prakash@iglulabs.com>
2015-08-09 18:39:54 +0530

    Promotions page creation

---
+++ app/src/main/java/com/findmycoach/app/activity/MyPromotions.java
@@ -0,0 +1,41 @@
+package com.findmycoach.app.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.findmycoach.app.R;
+
+public class MyPromotions extends ActionBarActivity {
+    PagerSlidingTabStrip pagerSlidingTabStrip;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_promotions);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_promotions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
